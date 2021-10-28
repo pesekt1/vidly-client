bb46435f8a382c3866a5d6808dd34a8143b2e47f
Tomas Pesek <pesekt@gmail.com>
2021-10-29 00:01:47 +0200

    login

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 class LoginForm extends React.Component {
   render() {
-    return <div>login</div>;
+    return (
+      <form>
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input type="text" className="form-control" id="username" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            placeholder="Password"
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    );
   }
 }
 
