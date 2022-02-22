e1cad204be68ce6eaed5554482f99596d7a925d0
Tomas Pesek <pesekt@gmail.com>
2022-02-22 14:27:37 +0100

    fixed navbar bug

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -8,6 +8,17 @@ class NavBar extends Component {
         <NavLink className="navbar-brand" to="/">
           Vidly
         </NavLink>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div className="navbar-nav">
             <NavLink className="nav-item nav-link" to="/movies">
